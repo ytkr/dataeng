5e249fbdf7a0024a8aae011df752c23788ca71a6
ytkr <teja.reddy@msg.com>
2019-09-21 15:02:28 +0530

    change version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='dataeng_msgtech',
-    version="1.0",
+    version="1.1",
     author='teja reddy',
     author_email='teja.reddy@msg.com',
     description="create dataeng pip package",
